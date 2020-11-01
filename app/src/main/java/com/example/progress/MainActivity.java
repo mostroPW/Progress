42480bee5e03d5003a83847dcd607de01cd01988
TYamada <MostroProprietorshipWorks@gmail.com>
2020-11-01 23:27:19 +0900

    プログレステスト

--- app/src/main/java/com/example/progress/MainActivity.java
+++ app/src/main/java/com/example/progress/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.progress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.view.MotionEvent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private ProgressBar progressBar;
     private int val = 0;
@@ -24,23 +27,25 @@ public class MainActivity extends AppCompatActivity {
         progressBar.setMax(1000);
 
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnTouchListener(new View.OnTouchListener() {
             @Override
-                    public void onClick(View v){
-                val += 1;
-                progressBar.setProgress(val);
-                progressBar.setSecondaryProgress(70);
+            public void onTouch(View v, MotionEvent event) {
+                //val += 1;
+                //progressBar.setProgress(val);
+                //progressBar.setSecondaryProgress(700);
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.v("OnTouch", "Touch Down");
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    Log.v("OnTouch", "Touch Up");
+                }
+                return false;
             }
         });
-
-
-
-
-
     }
-
-
-
 }
+
+
+
+
 
